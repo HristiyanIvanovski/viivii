804dc9135a367562558775ba9067887894e5d2cf
Hristiyan Ivanovski <hristiyan.ivanovski@valtech.com>
2025-06-17 20:14:31 +0200

    refactor: update query input method for dynamic user input

--- quick_rag.py
+++ quick_rag.py
@@ -4,8 +4,11 @@ client = weaviate.connect_to_local(port=8085)
 
 questions = client.collections.get("Question")
 
+#user input in terminal for query
+query = input("Enter your query: ")
+
 response = questions.generate.near_text(
-    query="science",
+    query=query,
     limit=1,
     grouped_task="Write a tweet about this fact. Do it in Elon Musk's style and voice, say something controversial. Don't use hashtags."
 )
